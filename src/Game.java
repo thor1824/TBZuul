d45c79bad54973c3caeb1dfe10477dfad97d6abd
thor1824 <42994659+thor1824@users.noreply.github.com>
2018-11-06 18:04:20 +0100

    Rette på implicit coupling

--- src/Game.java
+++ src/Game.java
@@ -1,30 +1,29 @@
+
 /**
- *  This class is the main class of the "World of Zuul" application. 
- *  "World of Zuul" is a very simple, text based adventure game.  Users 
- *  can walk around some scenery. That's all. It should really be extended 
+ *  This class is the main class of the "World of Zuul" application.
+ *  "World of Zuul" is a very simple, text based adventure game.  Users
+ *  can walk around some scenery. That's all. It should really be extended
  *  to make it more interesting!
- * 
+ *
  *  To play this game, create an instance of this class and call the "play"
  *  method.
- * 
+ *
  *  This main class creates and initialises all the others: it creates all
  *  rooms, creates the parser and starts the game.  It also evaluates and
  *  executes the commands that the parser returns.
- * 
+ *
  * @author  Michael Kölling and David J. Barnes
  * @version 2011.07.31
  */
+public class Game {
 
-public class Game 
-{
     private Parser parser;
     private Room currentRoom;
-        
+
     /**
      * Create the game and initialise its internal map.
      */
-    public Game() 
-    {
+    public Game() {
         createRooms();
         parser = new Parser();
     }
@@ -32,25 +31,21 @@ public class Game
     /**
      * Create all the rooms and link their exits together.
      */
-    private void createRooms()
-    {
+    private void createRooms() {
         Room entry, freskoChamber, aetherOrgan, downTheHole, relicOfTheFallen, poolRoom, storeRoom, eggRoom, statueRoom;
-        
+
         //creates the room
-        
-        entry = new Room("door"); 
-        freskoChamber = new Room("the fresko champer"); 
-        downTheHole = new Room(" down the hole"); 
+        entry = new Room("door");
+        freskoChamber = new Room("the fresko champer");
+        downTheHole = new Room(" down the hole");
         aetherOrgan = new Room("aether organ");
         relicOfTheFallen = new Room("Relic of the Fallen");
         poolRoom = new Room("The Pool Room");
         storeRoom = new Room("The Store Room");
         eggRoom = new Room("The Egg Room");
         statueRoom = new Room("The Statue Room");
-        
-        
+
         // initialise room exits
-        
         entry.setExit("decend", freskoChamber);
         freskoChamber.setExit("Straight", aetherOrgan);
         freskoChamber.setExit("left", relicOfTheFallen);
@@ -68,19 +63,17 @@ public class Game
 
         currentRoom = entry;  // start game outside
     }
-    
+
     /**
-     *  Main play routine.  Loops until end of play.
+     * Main play routine. Loops until end of play.
      */
-    public void play() 
-    {            
+    public void play() {
         printWelcome();
 
         // Enter the main command loop.  Here we repeatedly read commands and
         // execute them until the game is over.
-                
         boolean finished = false;
-        while (! finished) {
+        while (!finished) {
             Command command = parser.getCommand();
             finished = processCommand(command);
         }
@@ -90,8 +83,7 @@ public class Game
     /**
      * Print out the opening message for the player.
      */
-    private void printWelcome()
-    {
+    private void printWelcome() {
         System.out.println();
         System.out.println("Welcome to the World of Zuul!");
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
@@ -102,14 +94,14 @@ public class Game
 
     /**
      * Given a command, process (that is: execute) the command.
+     *
      * @param command The command to be processed.
      * @return true If the command ends the game, false otherwise.
      */
-    private boolean processCommand(Command command) 
-    {
+    private boolean processCommand(Command command) {
         boolean wantToQuit = false;
 
-        if(command.isUnknown()) {
+        if (command.isUnknown()) {
             System.out.println("I don't know what you mean...");
             return false;
         }
@@ -117,40 +109,34 @@ public class Game
         String commandWord = command.getCommandWord();
         if (commandWord.equals("help")) {
             printHelp();
-        }
-        else if (commandWord.equals("go")) {
+        } else if (commandWord.equals("go")) {
             goRoom(command);
-        }
-        else if (commandWord.equals("quit")) {
+        } else if (commandWord.equals("look")) {
+            look();
+        } else if (commandWord.equals("quit")) {
             wantToQuit = quit(command);
         }
-
         return wantToQuit;
     }
 
     // implementations of user commands:
-
     /**
-     * Print out some help information.
-     * Here we print some stupid, cryptic message and a list of the 
-     * command words.
+     * Print out some help information. Here we print some stupid, cryptic
+     * message and a list of the command words.
      */
-    private void printHelp() 
-    {
-        System.out.println("You are lost. You are alone. You wander");
-        System.out.println("around at the university.");
+    private void printHelp() {
         System.out.println();
         System.out.println("Your command words are:");
-        System.out.println("   go quit help");
+        System.out.println("Your command words are:");
+        parser.showCommands();
     }
 
-    /** 
-     * Try to go in one direction. If there is an exit, enter
-     * the new room, otherwise print an error message.
+    /**
+     * Try to go in one direction. If there is an exit, enter the new room,
+     * otherwise print an error message.
      */
-    private void goRoom(Command command) 
-    {
-        if(!command.hasSecondWord()) {
+    private void goRoom(Command command) {
+        if (!command.hasSecondWord()) {
             // if there is no second word, we don't know where to go...
             System.out.println("Go where?");
             return;
@@ -160,30 +146,35 @@ public class Game
 
         // Try to leave current room.
         Room nextRoom = currentRoom.getExit(direction);
-        
+
         if (nextRoom == null) {
             System.out.println("There is no door!");
-        }
-        else {
+        } else {
             currentRoom = nextRoom;
             System.out.println(currentRoom.getDescription());
             System.out.println(currentRoom.getExitString());
-            
+
         }
     }
 
-    /** 
-     * "Quit" was entered. Check the rest of the command to see
-     * whether we really quit the game.
+    /**
+     *
+     */
+    private void look() {
+        System.out.println(currentRoom.getLongDescription());
+    }
+
+    /**
+     * "Quit" was entered. Check the rest of the command to see whether we
+     * really quit the game.
+     *
      * @return true, if this command quits the game, false otherwise.
      */
-    private boolean quit(Command command) 
-    {
-        if(command.hasSecondWord()) {
+    private boolean quit(Command command) {
+        if (command.hasSecondWord()) {
             System.out.println("Quit what?");
             return false;
-        }
-        else {
+        } else {
             return true;  // signal that we want to quit
         }
     }
--- src/Room.java
+++ src/Room.java
@@ -78,4 +78,14 @@ public class Room {
         return description;
     }
 
+    /**
+     * Return a long description of this room, of the form: You are in the
+     * kitchen. Exits: north west
+     *
+     * @return A description of the room, including exits.
+     */
+    public String getLongDescription() {
+        return "You are " + description + ".\n" + getExitString();
+    }
+
 }
